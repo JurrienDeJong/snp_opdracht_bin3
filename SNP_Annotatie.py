2a90a021903a32ef440cf6f26c6530591fd385e2
JurrienDeJong <ju.de.jong@st.hanze.nl>
2021-10-11 14:39:52 +0200

    added stuff :)

--- SNP_Annotatie.py
+++ SNP_Annotatie.py
@@ -4,22 +4,23 @@ __author__ = "Jurrien de Jong"
 __date__ = "8/10/2021"
 __version__ = "V1.0"
 
-from Bio import SeqIO
+import re
 import sys
 import argparse
 from Bio import AlignIO
+from Bio import Align
 
-test = 5
 
-def align(file_name):
+def read_file(file_name):
     data = []
     if file_name.endswith(".msf"):
         alignment = AlignIO.read(open(file_name), "msf")
         for record in alignment:
-            data.append(record.seq)
+            data.append(record.seq.strip("\n"))
         return data
     else:
-        raise FileNotFoundError
+        raise Exception("An incorrect filetype is given."
+                        " Please hand over a .msf file")
 
 
 def dna_to_protein(sequence):
@@ -42,23 +43,57 @@ def dna_to_protein(sequence):
         'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
         'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
     }
-    if len(sequence) % 3 == 0:
+    if not re.match("[BDEFHIJKLMNOPQRSUVWXYZ]", sequence):
         for i in range(0, len(sequence), 3):
             codon = sequence[i:i + 3]
             protein_seq += table[codon]
-    return protein_seq
+        return protein_seq
+    else:
+        return sequence
+
+
+def implement_snp(seq, pos, snp):
+    if len(snp) > 1:
+        raise Exception("SNP should be one char long")
+    if pos in range(0, len(seq)):
+        sequence = seq[:pos] + snp + seq[pos + 1:]
+    else:
+        raise Exception("This index does not exist"
+                        " because the sequence is "
+                        "length: {}".format(len(seq)))
+    return sequence
+
+
+def get_align_score(msa, snp_seq):
+    score = []
+    aligner = Align.PairwiseAligner()
+    for x in range(0, len(msa)):
+        alignments = aligner.align(msa[x], )
+        score.append(alignments.score)
+    return score
+
 
 
 def main(arguments):
-    data = align(arguments.in_file)
+    msa = read_file(arguments.in_file)
     protein_seq = dna_to_protein(arguments.Sequence)
+    snp_seq = implement_snp(protein_seq,
+                            arguments.SNP_Pos,
+                            arguments.SNP)
+
+    # Append the sequence with the SNP
+    # to the msa for comparison
+    msa.append(snp_seq)
+
+    score = get_align_score(msa)
+    print(score)
     return 0
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process a MSA.')
     parser.add_argument('-i', type=str, dest='in_file', help='Please hand over a .msf file')
-    parser.add_argument('-seq', type=int, dest='Sequence', help='Please hand over a DNA or protein sequence.')
+    parser.add_argument('-seq', type=str, dest='Sequence', help='Please hand over a DNA or protein sequence.')
     parser.add_argument('-pos', type=int, dest='SNP_Pos', help='Please hand over a SNP position in the sequence.')
     parser.add_argument('-snp', type=str, dest='SNP', help='Please hand over a valid SNP.')
     arguments = parser.parse_args()
